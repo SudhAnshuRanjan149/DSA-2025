6c76bd9894fe180b9e4362801fbf8aa142cfb4f9
= <=>
2025-11-18 07:31:07 +0530

    add readme file for limkedList

---
+++ DSA-Basics/LinkedList/0_readme.js
@@ -0,0 +1,22 @@
+/* Linked List Theory
+
+- A linked list is a linear data structure where elements are stored in nodes.
+- Each node contains a value and a reference (or pointer) to the next node in the sequence.
+- Linked lists allow for efficient insertion and deletion of elements, as they do not require shifting of elements like arrays.
+- There are several types of linked lists, including singly linked lists, doubly linked lists, and circular linked lists.
+- Linked lists are commonly used in scenarios where dynamic memory allocation and frequent modifications are required.
+
+
+methods commonly associated with linked lists:-
+- push(value): Adds a new node with the specified value at the end of the linked list.
+- pop(): Removes the last node from the linked list and returns it.
+- unshift(value): Adds a new node with the specified value at the beginning of the linked list.
+- shift(): Removes the first node from the linked list and returns it.
+- get(index): Retrieves the node at the specified index in the linked list.
+- set(index, value): Updates the value of the node at the specified index in the linked list.
+- insert(index, value): Inserts a new node with the specified value at the specified index in the linked list.
+- remove(index): Removes the node at the specified index from the linked list and returns it.
+- printList(): Prints all the values in the linked list.
+
+
+*/
