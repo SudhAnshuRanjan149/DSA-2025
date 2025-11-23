a07d0977b6c798f8d5ee66d2d43dc3e34a94271f
= <sudhanshuranjan149@gmail.com>
2025-11-23 20:28:15 +0530

    add constructor for stacks

---
+++ DSA-Basics/StacksAndQueue/01_constructor.js
@@ -0,0 +1,68 @@
+/*
+
+stacks constructor using linked list
+
+*/
+
+
+// WRITE NODE CLASS HERE //
+class Node{
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Stack {
+	// WRITE STACK CONSTRUCTOR HERE //
+	constructor(value){
+	    let newNode = new Node(value)
+	    this.top = newNode;
+	    this.bottom = newNode;
+	    this.length = 1;
+	}
+
+    printStack() {
+        let temp = this.top;
+        while (temp !== null) {
+            console.log(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    getTop() {
+        if (this.top === null) {
+            console.log("Top: null");
+        } else {
+            console.log("Top: " + this.top.value);
+        }
+    }
+
+    getLength() {
+        console.log("Length: " + this.length);
+    }
+
+}
+ 
+
+ 
+let myStack = new Stack(4);
+
+myStack.getTop();
+myStack.getLength();
+
+console.log("\nStack:");
+myStack.printStack();
+
+
+/*
+    EXPECTED OUTPUT:
+    ----------------
+    Top: 4
+    Length: 1
+
+    Stack:
+    4
+
+*/
+ 
